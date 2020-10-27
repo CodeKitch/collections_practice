70893c3022cc7cdf9378312c5fe5a69b4ed6b459
JaSON <masteraccounthub@gmail.com>
2020-10-27 12:39:36 -0400

    commit tree f57b2b13f6938aaa3d143cffe716377293e949a8

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,48 @@
+def sort_array_asc(array)
+    array.sort
+end
+  
+def sort_array_desc(array)
+    array.sort.reverse
+end
 
+def sort_array_char_count(array)
+    array.sort_by! do |c|
+        c.length
+    end
+end
+
+def swap_elements(array)
+    array[1], array[2] = array[2], array[1]
+    array
+  end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(array)
+    array.each do |sym|
+        sym[2] = "$"
+    end
+end
+
+def find_a(array)
+    array.select do |sym|
+        sym[0] == "a"
+    end
+end
+
+def sum_array(array)
+    array.inject(0, :+)
+end
+
+def add_s(array)
+    array.map do |add|
+      if add != array[1]
+        add + "s"
+      else
+        add
+      end
+    end
+  end
